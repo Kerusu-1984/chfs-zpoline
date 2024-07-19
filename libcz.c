89cd6896e4272560e243d68c4ca9389550a149d2
Haruka Miyauchi <kerusu@yahoo.co.jp>
2024-07-19 22:11:53 +0900

    フック追加

--- libcz.c
+++ libcz.c
@@ -4,6 +4,7 @@
 #include <fcntl.h>
 #include <sys/syscall.h>
 #include <chfs.h>
+#include <stdlib.h>
 
 #define HOOK_FD_FLAG (1<<30)
 
@@ -18,7 +19,7 @@ static long hook_open(long a1, long a2, long a3,
     char *path = (char *)a2;
     int flags = (int)a3;
     mode_t mode = (mode_t)a4;
-    if (strncmp(path, "/chfs/", sizeof("/chfs/")) == 0) {
+    if (strncmp(path, "/chfs/", 6) == 0) {
         int ret;
         if (flags & O_CREAT) {
             ret = chfs_create(path, flags, mode);
@@ -58,13 +59,27 @@ static long hook_read(long a1, long a2, long a3,
     }
 }
 
+static long hook_write(long a1, long a2, long a3,
+			  long a4, long a5, long a6,
+			  long a7)
+{
+    int fd = (int)a2;
+    void *buf = (void *)a3;
+    size_t count = (size_t)a4;
+    if (fd & HOOK_FD_FLAG) {
+        return chfs_write(fd ^ HOOK_FD_FLAG, buf, count);
+    } else {
+        return next_sys_call(a1, a2, a3, a4, a5, a6, a7);
+    }
+}
+
 static long hook_stat(long a1, long a2, long a3,
 			  long a4, long a5, long a6,
 			  long a7)
 {
     char *path = (char *)a2;
     struct stat *st = (struct stat *)a3;
-    if (strncmp(path, "/chfs/", sizeof("/chfs/")) == 0) {
+    if (strncmp(path, "/chfs/", 6) == 0) {
         return chfs_stat(path, st);
     } else {
         return next_sys_call(a1, a2, a3, a4, a5, a6, a7);
@@ -101,6 +116,80 @@ static long hook_pwrite64(long a1, long a2, long a3,
     }
 }
 
+static long hook_openat(long a1, long a2, long a3,
+			  long a4, long a5, long a6,
+			  long a7)
+{
+    char *path = (char *)a3;
+    int flags = (int)a4;
+    mode_t mode = (mode_t)a5;
+    if (strncmp(path, "/chfs/", 6) == 0) {
+        int ret;
+        if (flags & O_CREAT) {
+            ret = chfs_create(path, flags, mode);
+        } else {
+            ret = chfs_open(path, flags);
+        }
+        if (ret < 0) return ret;
+        return ret | HOOK_FD_FLAG;
+    } else {
+        return next_sys_call(a1, a2, a3, a4, a5, a6, a7);
+    }
+}
+
+static long hook_lseek(long a1, long a2, long a3,
+			  long a4, long a5, long a6,
+			  long a7)
+{
+    int fd  = (int)a2;
+    off_t offset = (off_t)a3;
+    int whence = (int)a4;
+    if (fd & HOOK_FD_FLAG) {
+        return chfs_seek(fd ^ HOOK_FD_FLAG, offset, whence);
+    } else {
+        return next_sys_call(a1, a2, a3, a4, a5, a6, a7);
+    }
+}
+
+
+static long hook_fsync(long a1, long a2, long a3,
+			  long a4, long a5, long a6,
+			  long a7)
+{
+    int fd = (int)a2;
+    if (fd & HOOK_FD_FLAG) {
+        return chfs_fsync(fd ^ HOOK_FD_FLAG);
+    } else {
+        return next_sys_call(a1, a2, a3, a4, a5, a6, a7);
+    }
+}
+
+static long hook_lstat(long a1, long a2, long a3,
+			  long a4, long a5, long a6,
+			  long a7)
+{
+    char *path = (char *)a2;
+    struct stat *st = (struct stat *)a3;
+    if (strncmp(path, "/chfs/", 6) == 0) {
+        return chfs_stat(path, st);
+    } else {
+        return next_sys_call(a1, a2, a3, a4, a5, a6, a7);
+    }
+}
+
+static long hook_newfstatat(long a1, long a2, long a3,
+			  long a4, long a5, long a6,
+			  long a7)
+{
+    char *path = (char *)a3;
+    struct stat *buf = (struct stat *)a4;
+    if (strncmp(path, "/chfs/", 6) == 0) {
+        return chfs_stat(path, buf);
+    } else {
+        return next_sys_call(a1, a2, a3, a4, a5, a6, a7);
+    }
+}
+
 static long hook_function(long a1, long a2, long a3,
 			  long a4, long a5, long a6,
 			  long a7)
@@ -108,6 +197,8 @@ static long hook_function(long a1, long a2, long a3,
     switch (a1) {
         case SYS_read:
             return hook_read(a1, a2, a3, a4, a5, a6, a7);
+        case SYS_write:
+            return hook_write(a1, a2, a3, a4, a5, a6, a7);
         case SYS_open:
             return hook_open(a1, a2, a3, a4, a5, a6, a7);
         case SYS_close:
@@ -115,13 +206,19 @@ static long hook_function(long a1, long a2, long a3,
         case SYS_stat:
             return hook_stat(a1, a2, a3, a4, a5, a6, a7);
         case SYS_lstat:
-            break;
+            return hook_lstat(a1, a2, a3, a4, a5, a6, a7);
         case SYS_lseek:
-            break;
+            return hook_lseek(a1, a2, a3, a4, a5, a6, a7);
         case SYS_pread64:
             return hook_pread64(a1, a2, a3, a4, a5, a6, a7);
         case SYS_pwrite64:
             return hook_pwrite64(a1, a2, a3, a4, a5, a6, a7);
+        case SYS_openat:
+            return hook_openat(a1, a2, a3, a4, a5, a6, a7);
+        case SYS_fsync:
+            return hook_fsync(a1, a2, a3, a4, a5, a6, a7);
+        case SYS_newfstatat:
+            return hook_newfstatat(a1, a2, a3, a4, a5, a6, a7);
         default:
             break;
     }
