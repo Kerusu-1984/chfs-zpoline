42c942d36c6f9e0de541f10dfdac1253de21d29f
Osamu Tatebe <tatebe@cs.tsukuba.ac.jp>
2025-01-03 03:50:15 +0900

    sequel to ee273ab137e3aa01cabe160a60d9829cfaaf3f5b. syscall numbers are not consecutive

--- syscall_string.sh
+++ syscall_string.sh
@@ -2,13 +2,16 @@
 
 grep __NR_ /usr/include/x86_64-linux-gnu/asm/unistd_64.h |
 	awk 'BEGIN { print "static char* syscall_list[] = {" }
-	     { print "/* " $3 " */ \"" $2 "\"," }
+	     { print "[" $3 "] = \"" $2 "\"," }
 	     END { print "};" }'
 
 cat <<EOF
 
 const char *syscall_string(int num)
 {
+	if (num < 0 || num >= sizeof(syscall_list) / sizeof(syscall_list[0]) ||
+			syscall_list[num] == 0)
+		return ("unknown");
 	return (syscall_list[num]);
 }
 EOF
