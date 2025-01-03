cffc54288d397afc6506a2382de3bb8db29bb4ab
Osamu Tatebe <tatebe@cs.tsukuba.ac.jp>
2025-01-04 00:17:33 +0900

    (syscall_string.sh) style change

--- syscall_string.sh
+++ syscall_string.sh
@@ -2,13 +2,14 @@
 
 grep __NR_ /usr/include/x86_64-linux-gnu/asm/unistd_64.h |
 	sed 's/__NR_//' |
-	awk 'BEGIN { print "static char* syscall_list[] = {" }
+	awk 'BEGIN { print "static char *syscall_list[] = {" }
 	     { print "[" $3 "] = \"" $2 "\"," }
 	     END { print "};" }'
 
 cat <<EOF
 
-const char *syscall_string(int num)
+const char *
+syscall_string(int num)
 {
 	if (num < 0 || num >= sizeof(syscall_list) / sizeof(syscall_list[0]) ||
 			syscall_list[num] == 0)
